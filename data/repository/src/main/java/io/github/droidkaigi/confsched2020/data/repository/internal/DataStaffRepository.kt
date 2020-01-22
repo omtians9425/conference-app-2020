43cba2a4909ee2b45ce88df357b1ffd34dc80179
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-22 21:20:37 +0900

    finish introducing store4

--- data/repository/src/main/java/io/github/droidkaigi/confsched2020/data/repository/internal/DataStaffRepository.kt
+++ data/repository/src/main/java/io/github/droidkaigi/confsched2020/data/repository/internal/DataStaffRepository.kt
@@ -1,11 +1,18 @@
 package io.github.droidkaigi.confsched2020.data.repository.internal
 
+import com.dropbox.android.external.store4.MemoryPolicy
+import com.dropbox.android.external.store4.StoreBuilder
+import com.dropbox.android.external.store4.StoreRequest
+import com.dropbox.android.external.store4.StoreResponse
 import io.github.droidkaigi.confsched2020.data.api.DroidKaigiApi
+import io.github.droidkaigi.confsched2020.data.api.response.StaffResponse
 import io.github.droidkaigi.confsched2020.data.db.StaffDatabase
 import io.github.droidkaigi.confsched2020.data.db.entity.StaffEntity
 import io.github.droidkaigi.confsched2020.model.repository.StaffRepository
 import io.github.droidkaigi.confsched2020.model.Staff
 import io.github.droidkaigi.confsched2020.model.StaffContents
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -14,14 +21,30 @@ class DataStaffRepository @Inject constructor(
     private val staffDatabase: StaffDatabase
 ) : StaffRepository {
 
+    val store = StoreBuilder.fromNonFlow<Unit, StaffResponse> { api.getStaffs() }
+        .persister(
+            reader = { readFromLocal() },
+            writer = { _: Unit, output: StaffResponse -> staffDatabase.save(output) }
+        )
+        .cachePolicy(MemoryPolicy.builder().build())
+        .build()
+
     override suspend fun refresh() {
         val response = api.getStaffs()
         staffDatabase.save(response)
     }
 
+    fun readFromLocal() = staffDatabase
+        .staffs()
+        .map { StaffContents(it.map { staffEntity -> staffEntity.toStaff() }) }
+
+    fun staffsStore(): Flow<StoreResponse<StaffContents>> {
+        return store.stream(StoreRequest.cached(key = Unit, refresh = true))
+    }
+
     override fun staffs() = staffDatabase
         .staffs()
-        .map { StaffContents(it.map { staffEntity -> staffEntity.toStaff() } )}
+        .map { StaffContents(it.map { staffEntity -> staffEntity.toStaff() }) }
 }
 
 private fun StaffEntity.toStaff(): Staff = Staff(id, name, iconUrl, profileUrl)
--- feature/staff/src/main/java/io/github/droidkaigi/confsched2020/staff/ui/viewmodel/StaffsViewModel.kt
+++ feature/staff/src/main/java/io/github/droidkaigi/confsched2020/staff/ui/viewmodel/StaffsViewModel.kt
@@ -1,21 +1,41 @@
 package io.github.droidkaigi.confsched2020.staff.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.liveData
+import com.dropbox.android.external.store4.MemoryPolicy
+import com.dropbox.android.external.store4.StoreBuilder
+import com.dropbox.android.external.store4.StoreRequest
+import com.dropbox.android.external.store4.StoreResponse
+import io.github.droidkaigi.confsched2020.data.api.DroidKaigiApi
+import io.github.droidkaigi.confsched2020.data.api.response.StaffResponse
+import io.github.droidkaigi.confsched2020.data.db.StaffDatabase
+import io.github.droidkaigi.confsched2020.data.db.entity.StaffEntity
+import io.github.droidkaigi.confsched2020.data.repository.internal.DataStaffRepository
 import io.github.droidkaigi.confsched2020.ext.combine
 import io.github.droidkaigi.confsched2020.ext.toAppError
-import io.github.droidkaigi.confsched2020.ext.toLoadingState
 import io.github.droidkaigi.confsched2020.model.AppError
-import io.github.droidkaigi.confsched2020.model.LoadState
+import io.github.droidkaigi.confsched2020.model.Staff
 import io.github.droidkaigi.confsched2020.model.StaffContents
-import io.github.droidkaigi.confsched2020.model.repository.StaffRepository
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+@FlowPreview
 class StaffsViewModel @Inject constructor(
-    private val staffRepository: StaffRepository
+    private val api: DroidKaigiApi,
+    private val staffDatabase: StaffDatabase,
+    private val staffRepository: DataStaffRepository
 ) : ViewModel() {
+
+    private val store = StoreBuilder.fromNonFlow<Unit, StaffResponse> { api.getStaffs() }
+        .persister(
+            reader = { readFromLocal() },
+            writer = { _: Unit, output: StaffResponse -> staffDatabase.save(output) }
+        )
+        .cachePolicy(MemoryPolicy.builder().build())
+        .build()
 
     data class UiModel(
         val isLoading: Boolean,
@@ -27,21 +47,44 @@ class StaffsViewModel @Inject constructor(
         }
     }
 
-    private val staffContentsLoadState: LiveData<LoadState<StaffContents>> = liveData {
-        emitSource(
-            staffRepository.staffs()
-                .toLoadingState()
-                .asLiveData()
-        )
-        staffRepository.refresh()
-    }
+    private val staffContentsLoadState: LiveData<StoreResponse<StaffContents>> =
+        store.stream(StoreRequest.cached(key = Unit, refresh = true)).asLiveData()
+
+//    private val staffContentsLoadState: LiveData<LoadState<StaffContents>> = liveData {
+//        emitSource(
+//            staffRepository.staffs()
+//                .toLoadingState()
+//                .asLiveData()
+//        )
+//        staffRepository.refresh()
+//    }
+//
+//    val uiModel: LiveData<UiModel> = combine(
+//        initialValue = UiModel.EMPTY,
+//        liveData1 = staffContentsLoadState
+//    ) { _, loadState ->
+//        val staffContents = when (loadState) {
+//            is LoadState.Loaded -> {
+//                loadState.value
+//            }
+//            else -> {
+//                StaffContents.EMPTY
+//            }
+//        }
+//        UiModel(
+//            isLoading = loadState.isLoading,
+//            error = loadState.getErrorIfExists().toAppError(),
+//            staffContents = staffContents
+//        )
+//    }
 
     val uiModel: LiveData<UiModel> = combine(
         initialValue = UiModel.EMPTY,
         liveData1 = staffContentsLoadState
     ) { _, loadState ->
+        Log.d("StaffsViewModel", "combine. data source : ${loadState.origin}, data : $loadState")
         val staffContents = when (loadState) {
-            is LoadState.Loaded -> {
+            is StoreResponse.Data -> {
                 loadState.value
             }
             else -> {
@@ -49,10 +92,15 @@ class StaffsViewModel @Inject constructor(
             }
         }
         UiModel(
-            isLoading = loadState.isLoading,
-            error = loadState.getErrorIfExists().toAppError(),
+            isLoading = loadState is StoreResponse.Loading,
+            error = loadState.errorOrNull().toAppError(),
             staffContents = staffContents
         )
     }
 
+    private fun readFromLocal() = staffDatabase
+        .staffs()
+        .map { StaffContents(it.map { staffEntity -> staffEntity.toStaff() }) }
+
+    private fun StaffEntity.toStaff(): Staff = Staff(id, name, iconUrl, profileUrl)
 }
